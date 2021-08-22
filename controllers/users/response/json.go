682525e32a69734af183e1f7faf12d9e13329415
Yudha Pradana <yudhaseptiandry@gmail.com>
2021-08-22 21:47:14 +0700

    collection postman

--- controllers/users/response/json.go
+++ controllers/users/response/json.go
@@ -7,9 +7,9 @@ import (
 
 type User struct {
 	ID        uint      `json:"id"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
-	DeletedAt time.Time `json:"deletedAt"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
 	//Token     string    `json:"token"`
--- drivers/databases/transactions/mysql.go
+++ drivers/databases/transactions/mysql.go
@@ -31,7 +31,7 @@ func (repository *MysqlTransactionRepository) Store(ctx context.Context, transac
 func (repository *MysqlTransactionRepository) GetTrx(ctx context.Context, userID int) ([]transactions.Domain, error) {
 	rec := []Transaction{}
 
-	err := repository.Conn.Preload("Users").Preload("Notes").Preload("Notes.Categories").Preload("Notes.Users").Where("transactions.user_id = ?", userID).Find(&rec).Error
+	err := repository.Conn.Preload("Users").Preload("Notes").Where("transactions.user_id = ?", userID).Find(&rec).Error
 	//err := repository.Conn.Preload(clause.Associations).Where("transactions.user_id = ?", userID).Find(&rec).Error
 	if err != nil {
 		return []transactions.Domain{}, err
